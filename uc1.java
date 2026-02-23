5a7f6cf7bb7792e153f7abebe7dde4b566edb156
Yesudhas M <ynsameera@gmail.com>
2026-02-23 11:11:24 +0530

    first usercase

---
+++ uc1.java
@@ -0,0 +1,11 @@
+class demo2 {
+    public static void main(String args[]){
+        System.out.println("****	****	****	****");
+		System.out.println("*  *	*  *	*  *	*");
+		System.out.println("*  *	*  *	*  *	*");
+		System.out.println("*  *	*  *	****	****");
+		System.out.println("*  *	*  *	*			*");
+		System.out.println("*  *	*  *	*			*");
+		System.out.println("****	****	*		*****");
+    }
+}
